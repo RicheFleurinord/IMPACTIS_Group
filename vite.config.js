9e462d4f11d3bf347c2106e4976d7c5541da9657
Abdielson <abdueljhon@gmail.com>
2026-05-17 16:26:47 -0400

    Fix gitignore and build production

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default {
+export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
       onwarn(warning, warn) {
@@ -11,4 +11,4 @@ export default {
       }
     }
   }
-}
+})
